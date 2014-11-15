93ee2b48ecd69f17e79dd1b045fa25b6ce94a43f
Joe Doherty <joe@pjuu.com>
2014-11-15 07:51:20 +0000

    Cleaned up failures for GET requests...
    We can't do it for POSTs because we can't catch them with WebView or WebViewClient. We will have to come up with a solution to this.
    
    Added a spinner when the page is loading for a bit of visual feed back.
    
    This will be the first released version of the Android app.

--- app/src/main/java/com/pjuu/droidotter/MainActivity.java
+++ app/src/main/java/com/pjuu/droidotter/MainActivity.java
@@ -1,13 +1,65 @@
 package com.pjuu.droidotter;
 
 import android.app.Activity;
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
+import android.view.View;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.view.Window;
+import android.widget.ProgressBar;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
+
+    private ProgressBar prgrsBar;
+
+    public Boolean hasConnection() {
+        // Get a ConnectivityManager
+        ConnectivityManager cm =
+                (ConnectivityManager)getApplicationContext()
+                        .getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo activeNetwork = cm.getActiveNetworkInfo();
+        return activeNetwork != null && activeNetwork.isConnectedOrConnecting();
+    }
+
+    public class PjuuWebViewClient extends WebViewClient {
+
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            if (hasConnection()) {
+                view.loadUrl(url);
+                return true;
+            } else {
+                Toast.makeText(getApplicationContext(), "No connection", Toast.LENGTH_SHORT).show();
+                return true;
+            }
+        }
+
+        @Override
+        public void onPageStarted(WebView view, String url, Bitmap favicon) {
+            super.onPageStarted(view, url, favicon);
+            prgrsBar.setVisibility(View.VISIBLE);
+        }
+
+        @Override
+        public void onPageFinished(WebView view, String url) {
+            super.onPageFinished(view, url);
+            prgrsBar.setVisibility(View.GONE);
+        }
+
+        @Override
+        public void onReceivedError(WebView view, int errorCode,
+                                    String description, String failingUrl) {
+            // Will display a standard error message if the application can't connect
+            view.loadUrl("file:///android_asset/error.html");
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,31 +74,16 @@ public class MainActivity extends Activity {
         /*
          * WebView stuff
          */
-        WebView browser = (WebView)findViewById(R.id.webview);
-        WebSettings browserSettings = browser.getSettings();
+        WebView webView = (WebView)findViewById(R.id.webview);
+        WebSettings browserSettings = webView.getSettings();
         // We need Javascript on the site
         browserSettings.setJavaScriptEnabled(true);
-        browser.setWebViewClient(new Browser());
-        // The URL is stores in values/strings.xml
-        browser.loadUrl(getString(R.string.app_url));
+        webView.setWebViewClient(new PjuuWebViewClient());
+        // The URL is stored in values/strings.xml
+        webView.loadUrl(getString(R.string.app_url));
+
+        // Progress bar
+        prgrsBar = (ProgressBar)findViewById(R.id.progress_bar);
     }
 
-    private class Browser extends WebViewClient {
-        /*
-         * Slightly customized version of WebViewClient
-         */
-
-        @Override
-        public boolean shouldOverrideUrlLoading(WebView view, String url) {
-            view.loadUrl(url);
-            return true;
-        }
-
-        @Override
-        public void onReceivedError(WebView view, int errorCode,
-                                    String description, String failingUrl) {
-            // Will display a standard error message if the application can't connect
-            view.loadUrl("file:///android_asset/error.html");
-        }
-    }
 }
