e6bcd3fcc71ca5e82cd0d24eed79c8e0dcbf85c5
Joe Doherty <joe@pjuu.com>
2015-02-04 08:05:24 +0000

    Links that are not "pjuu.com" links are now opened in the default browser via an Intent. Closes #1.

--- app/src/main/java/com/pjuu/droidotter/MainActivity.java
+++ app/src/main/java/com/pjuu/droidotter/MainActivity.java
@@ -2,9 +2,11 @@ package com.pjuu.droidotter;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
 import android.graphics.Bitmap;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.KeyEvent;
 import android.view.View;
@@ -15,6 +17,9 @@ import android.webkit.WebViewClient;
 import android.view.Window;
 import android.widget.ProgressBar;
 import android.widget.Toast;
+
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class MainActivity extends Activity {
 
@@ -63,12 +68,16 @@ public class MainActivity extends Activity {
         @Override
         public boolean shouldOverrideUrlLoading(WebView view, String url) {
             if (hasConnection()) {
-                view.loadUrl(url);
-                return true;
+                if (url.contains("pjuu.com")) {
+                    view.loadUrl(url);
+                } else {
+                    Intent i = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+                    startActivity(i);
+                }
             } else {
                 Toast.makeText(getApplicationContext(), "No connection", Toast.LENGTH_SHORT).show();
-                return true;
             }
+            return true;
         }
 
         @Override
