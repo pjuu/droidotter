40ee7d60e767ebcd838b08842ad03bb33a5d7fa4
Joe Doherty <joe@pjuu.com>
2015-02-10 07:52:42 +0000

    Added support for fileupload in ICS, Jellybean and Lollipop. KitKat doesn't work.

--- app/src/main/java/com/pjuu/droidotter/MainActivity.java
+++ app/src/main/java/com/pjuu/droidotter/MainActivity.java
@@ -1,5 +1,6 @@
 package com.pjuu.droidotter;
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
@@ -8,10 +9,6 @@ import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.net.Uri;
 import android.os.Bundle;
-import android.os.Environment;
-import android.os.Parcelable;
-import android.provider.MediaStore;
-import android.util.Log;
 import android.view.View;
 import android.webkit.ValueCallback;
 import android.webkit.WebChromeClient;
@@ -22,20 +19,46 @@ import android.view.Window;
 import android.widget.ProgressBar;
 import android.widget.Toast;
 
-import java.io.File;
-import java.io.IOException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-
 public class MainActivity extends Activity {
+
+    protected static final int REQUEST_CODE_FILE_PICKER = 1;
 
     private WebView webView;
     private ProgressBar prgrsBar;
+
+    /** File upload callback for platform versions prior to Android 5.0 */
+    protected ValueCallback<Uri> mFileUploadCallbackFirst;
+    /** File upload callback for Android 5.0+ */
+    protected ValueCallback<Uri[]> mFileUploadCallbackSecond;
 
     // Hostname extracted from R.string.app_url
     private String hostname;
 
     public class PjuuWebChromeClient extends WebChromeClient {
+
+        // file upload callback (Android 2.2 (API level 8) -- Android 2.3 (API level 10)) (hidden method)
+        @SuppressWarnings("unused")
+        public void openFileChooser(ValueCallback<Uri> uploadMsg) {
+            openFileChooser(uploadMsg, null);
+        }
+
+        // file upload callback (Android 3.0 (API level 11) -- Android 4.0 (API level 15)) (hidden method)
+        public void openFileChooser(ValueCallback<Uri> uploadMsg, String acceptType) {
+            openFileChooser(uploadMsg, acceptType, null);
+        }
+
+        // file upload callback (Android 4.1 (API level 16) -- Android 4.3 (API level 18)) (hidden method)
+        @SuppressWarnings("unused")
+        public void openFileChooser(ValueCallback<Uri> uploadMsg, String acceptType, String capture) {
+            openFileInput(uploadMsg, null);
+        }
+
+        // file upload callback (Android 5.0 (API level 21) -- current) (public method)
+        @SuppressWarnings("all")
+        public boolean onShowFileChooser(WebView webView, ValueCallback<Uri[]> filePathCallback, WebChromeClient.FileChooserParams fileChooserParams) {
+            openFileInput(null, filePathCallback);
+            return true;
+        }
 
     }
 
@@ -81,13 +104,65 @@ public class MainActivity extends Activity {
         }
     }
 
-    public Boolean hasConnection() {
+    public boolean hasConnection() {
         // Get a ConnectivityManager
         ConnectivityManager cm =
                 (ConnectivityManager)getApplicationContext()
                         .getSystemService(Context.CONNECTIVITY_SERVICE);
         NetworkInfo activeNetwork = cm.getActiveNetworkInfo();
         return activeNetwork != null && activeNetwork.isConnectedOrConnecting();
+    }
+
+    @SuppressLint("NewApi")
+    protected void openFileInput(final ValueCallback<Uri> fileUploadCallbackFirst,
+                                 final ValueCallback<Uri[]> fileUploadCallbackSecond) {
+        if (mFileUploadCallbackFirst != null) {
+            mFileUploadCallbackFirst.onReceiveValue(null);
+        }
+        mFileUploadCallbackFirst = fileUploadCallbackFirst;
+        if (mFileUploadCallbackSecond != null) {
+            mFileUploadCallbackSecond.onReceiveValue(null);
+        }
+        mFileUploadCallbackSecond = fileUploadCallbackSecond;
+        Intent i = new Intent(Intent.ACTION_GET_CONTENT);
+        i.addCategory(Intent.CATEGORY_OPENABLE);
+        i.setType("image/*");
+        startActivityForResult(Intent.createChooser(i, "Choose a file"),
+                    REQUEST_CODE_FILE_PICKER);
+    }
+
+    public void onActivityResult(final int requestCode, final int resultCode, final Intent intent) {
+        if (requestCode == REQUEST_CODE_FILE_PICKER) {
+            if (resultCode == Activity.RESULT_OK) {
+                if (intent != null) {
+                    if (mFileUploadCallbackFirst != null) {
+                        mFileUploadCallbackFirst.onReceiveValue(intent.getData());
+                        mFileUploadCallbackFirst = null;
+                    }
+                    else if (mFileUploadCallbackSecond != null) {
+                        Uri[] dataUris;
+                        try {
+                            dataUris = new Uri[] { Uri.parse(intent.getDataString()) };
+                        }
+                        catch (Exception e) {
+                            dataUris = null;
+                        }
+                        mFileUploadCallbackSecond.onReceiveValue(dataUris);
+                        mFileUploadCallbackSecond = null;
+                    }
+                }
+            }
+            else {
+                if (mFileUploadCallbackFirst != null) {
+                    mFileUploadCallbackFirst.onReceiveValue(null);
+                    mFileUploadCallbackFirst = null;
+                }
+                else if (mFileUploadCallbackSecond != null) {
+                    mFileUploadCallbackSecond.onReceiveValue(null);
+                    mFileUploadCallbackSecond = null;
+                }
+            }
+        }
     }
 
     @Override
@@ -101,19 +176,14 @@ public class MainActivity extends Activity {
         }
     }
 
+    @SuppressLint("SetJavaScriptEnabled")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        /*
-         * Activity stuff
-         */
         this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main);
 
-        /*
-         * WebView stuff
-         */
         // Get the hostname from the URL defined in the resources
         // This will let us check all requests to redirect to the browser or not.
         hostname = Uri.parse(getString(R.string.app_url)).getHost();
